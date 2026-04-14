660ca686a9a0a934f477258f646e00790a33bcab
Dovietmy5903 <dovietmy05092k3@gmail.com>
2026-04-14 15:43:27 +0700

    create base entity and user role

---
+++ src/main/java/com/example/demo/model/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class BaseEntity {
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public LocalDateTime getUpdatedAt() { return updatedAt; }
+}
