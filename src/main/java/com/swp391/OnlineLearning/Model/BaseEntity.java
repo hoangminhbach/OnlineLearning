ed2f9947d7a3a58dee2b517136fbb0aa59e34826
Huy <hn662945@gmail.com>
2026-04-15 13:26:54 +0700

    add BaseEntity entities

--- src/main/java/com/swp391/OnlineLearning/Model/BaseEntity.java
+++ src/main/java/com/swp391/OnlineLearning/Model/BaseEntity.java
@@ -1,10 +1,20 @@
 package com.swp391.OnlineLearning.Model;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDateTime;
+@Getter
 @MappedSuperclass
 public abstract class BaseEntity {
+    // Getter
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
 }
