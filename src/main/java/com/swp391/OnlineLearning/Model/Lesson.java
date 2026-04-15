39a3fbda53a593a27c4d5c78b16822431323105d
Huy <hn662945@gmail.com>
2026-04-15 15:12:01 +0700

    create model Lesson

---
+++ src/main/java/com/swp391/OnlineLearning/Model/Lesson.java
@@ -0,0 +1,8 @@
+package com.swp391.OnlineLearning.Model;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import java.util.ArrayList;
+import java.util.List;
+public class Lesson {
+}
