30dc3477dbca00155ca6c00afcec4fb384665dbb
Huy <hn662945@gmail.com>
2026-04-18 14:44:24 +0700

    adjust EnrolmentImpl

--- src/main/java/com/swp391/OnlineLearning/Service/impl/EnrollmentServiceImpl.java
+++ src/main/java/com/swp391/OnlineLearning/Service/impl/EnrollmentServiceImpl.java
@@ -1,13 +1,13 @@
 package com.swp391.OnlineLearning.Service.impl;
 
-import com.swp391.OnlineEnglishLearningSystem.model.Enrollment;
-import com.swp391.OnlineEnglishLearningSystem.model.Order;
-import com.swp391.OnlineEnglishLearningSystem.model.User;
-import com.swp391.OnlineEnglishLearningSystem.model.UserLesson;
-import com.swp391.OnlineEnglishLearningSystem.model.dto.EnrollmentInfoDTO;
-import com.swp391.OnlineEnglishLearningSystem.model.dto.EnrollmentLearningDTO;
-import com.swp391.OnlineEnglishLearningSystem.repository.EnrollmentRepository;
-import com.swp391.OnlineEnglishLearningSystem.service.EnrollmentService;
+import com.swp391.OnlineLearning.model.Enrollment;
+import com.swp391.OnlineLearning.model.Order;
+import com.swp391.OnlineLearning.model.User;
+import com.swp391.OnlineLearning.model.UserLesson;
+import com.swp391.OnlineLearning.model.dto.EnrollmentInfoDTO;
+import com.swp391.OnlineLearning.model.dto.EnrollmentLearningDTO;
+import com.swp391.OnlineLearning.repository.EnrollmentRepository;
+import com.swp391.OnlineLearning.service.EnrollmentService;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
