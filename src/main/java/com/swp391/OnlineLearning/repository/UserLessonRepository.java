00a049a1a417d031cc9d5a6190644e4ee915e61f
Tên của bạn <q314e25r@gmail.com>
2026-04-18 14:16:59 +0700

    update repository

---
+++ src/main/java/com/swp391/OnlineLearning/repository/UserLessonRepository.java
@@ -0,0 +1,17 @@
+package com.swp391.OnlineLearning.repository;
+
+import com.swp391.OnlineLearning.model.UserLesson;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserLessonRepository extends JpaRepository<UserLesson, Long> {
+    Optional<UserLesson> findByUserIdAndLessonId(long userId, long lessonId);
+
+    @Query("SELECT ul FROM UserLesson ul JOIN FETCH ul.lesson " +
+            "WHERE ul.enrollment.id = :enrollmentId AND ul.user.id = :userId")
+    List<UserLesson> findAllByEnrollmentIdAndUserIdWithLesson(@Param("enrollmentId") long enrollmentId, @Param("userId") long userId);
+}
