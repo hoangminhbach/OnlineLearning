80522d8ed44c11438895fa1a9e276881d9bbcb23
Nhật Huy <q314e25r@gmail.com>
2026-04-19 14:53:10 +0700

    Delete src/main/java/com/swp391/OnlineLearning/service directory
    remove duplicate lowercase controller folder

--- src/main/java/com/swp391/OnlineLearning/service/LearningService.java
+++
@@ -1,9 +0,0 @@
-package com.swp391.OnlineLearning.service;
-
-import com.swp391.OnlineLearning.model.dto.ChapterLearningDTO;
-
-import java.util.List;
-
-public interface LearningService {
-    List<ChapterLearningDTO> prepareLearningViewData(long userId, long enrollmentId);
-}
